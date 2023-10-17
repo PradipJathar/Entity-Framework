994b5f6d42d149a560ed98b98195fc2d1a2037e3
PradipJathar <jatharpradipofficial@gmail.com>
2023-10-17 18:21:24 +0530

    Section - 3 - Solution - Database First

---
+++ Section3_ExerciseSolution_DatabaseFirst/Section3_ExerciseSolution_DatabaseFirst/Classification.cs
@@ -0,0 +1,9 @@
+﻿namespace Section3_ExerciseSolution_DatabaseFirst
+{
+    public enum Classification : byte
+    {
+        Silver = 1, 
+        Gold = 2,
+        Platinum = 3
+    }
+}
---
+++ Section3_ExerciseSolution_DatabaseFirst/Section3_ExerciseSolution_DatabaseFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section3_ExerciseSolution_DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VidzyDbContext db = new VidzyDbContext();
+
+            db.AddVideo("Ra.One", DateTime.Today, "Action", (byte)Classification.Silver);
+            db.AddVideo("2.O", DateTime.Today, "Thriller", (byte)Classification.Platinum);
+            db.AddVideo("Hera Pheri", DateTime.Today, "Comedy", (byte)Classification.Gold);
+            db.SaveChanges();
+        }
+    }
+}
