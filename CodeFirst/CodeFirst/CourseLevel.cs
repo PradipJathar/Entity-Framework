0c7ab7deeea94c7601e582c67ecda60602c23132
PradipJathar <jatharpradipofficial@gmail.com>
2023-10-17 19:13:36 +0530

    Code First with a New Database.

---
+++ CodeFirst/CodeFirst/CourseLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeFirst
+{
+    public enum CourseLevel : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+}
---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class PlutoCodeFirstDbContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public PlutoCodeFirstDbContext() : base("name=DefaultConnection")
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
