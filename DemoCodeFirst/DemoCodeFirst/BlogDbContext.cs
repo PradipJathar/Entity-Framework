013636106dcfda05d713cdb4f628e7845a62ae92
PradipJathar <jatharpradipofficial@gmail.com>
2023-10-17 13:27:12 +0530

    Demo - Code First Workflow.

---
+++ DemoCodeFirst/DemoCodeFirst/BlogDbContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace DemoCodeFirst
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ DemoCodeFirst/DemoCodeFirst/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCodeFirst
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlogDbContext db = new BlogDbContext();
+
+            Post post = new Post()
+            {
+                DatePublished = DateTime.Now,
+                Title = "Title",
+                Body = "Body"
+            };
+
+            db.Posts.Add(post);
+            db.SaveChanges();
+        }
+    }
+}
